a19b3fd9c64784a68b92c2c98b84b93ab23f2baf
rajeev-k47 <rajeevkumawat2007@outlook.com>
2024-06-04 02:10:49 +0530

    <Adds Parameters in the Data Class>?

--- app/src/main/java/com/example/ruine/Rvmodel.kt
+++ app/src/main/java/com/example/ruine/Rvmodel.kt
@@ -1,4 +1,4 @@
 package com.example.ruine
 
-data class Rvmodel(var profile:Int?=R.drawable.google,var name: String?="", var mail_tag: String?="") {
+data class  Rvmodel(var profile:Int?=R.drawable.group,var name: String?="", var mail_tag: String?="",val grp_id:String?="") {
 }
