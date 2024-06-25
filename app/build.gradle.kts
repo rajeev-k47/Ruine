9d8a17e26deed14ded1833659cdc99885bedeba2
rajeev-k47 <rajeevkumawat2007@outlook.com>
2024-06-25 19:23:13 +0530

    <*/*>?

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,11 +81,15 @@ dependencies {
 
     val room_version = "2.6.1"
 
-    implementation("androidx.room:room-runtime:$room_version")
-    annotationProcessor("androidx.room:room-compiler:$room_version")
-    implementation("androidx.room:room-ktx:$room_version")
-    kapt("androidx.room:room-compiler:$room_version")
+    implementation(libs.androidx.room.runtime)
+    annotationProcessor(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.ktx)
+    kapt(libs.androidx.room.compiler)
+    implementation (libs.glide)
+    kapt (libs.compiler)
+    implementation (libs.glide.transformations)
     // To use Kotlin Symbol Processing (KSP)
 //    ksp("androidx.room:room-compiler:$room_version")
+
 
 }
