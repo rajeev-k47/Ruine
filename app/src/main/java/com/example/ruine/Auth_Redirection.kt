c8e6e793209e0c563f52dd9b0ff6d82cddad6700
rajeev-k47 <rajeevkumawat2007@outlook.com>
2024-06-30 05:36:47 +0530

    <*/*>?

--- app/src/main/java/com/example/ruine/Auth_Redirection.kt
+++ app/src/main/java/com/example/ruine/Auth_Redirection.kt
@@ -30,7 +30,7 @@ class Auth_Redirection : AppCompatActivity() {
         val encodedScopes = scopes.joinToString(separator = "%20") { Uri.encode(it) }
 
 
-        val url = "https://accounts.google.com/o/oauth2/v2/auth?scope=$encodedScopes&access_type=offline&include_granted_scopes=true&prompt=consent&response_type=code&state=state_parameter_passthrough_value&redirect_uri=https://ruine-credentials.onrender.com/callback&client_id=${getString(R.string.client_id)}&flowName=GeneralOAuthFlow"
+        val url = "https://accounts.google.com/o/oauth2/v2/auth?scope=$encodedScopes&access_type=offline&include_granted_scopes=true&prompt=consent&response_type=code&state=state_parameter_passthrough_value&redirect_uri=https://ruine-credentials-chu6.onrender.com/callback&client_id=${getString(R.string.client_id)}&flowName=GeneralOAuthFlow"
 
         val customTabsIntent = CustomTabsIntent.Builder().build()
         customTabsIntent.launchUrl(this, Uri.parse(url))
