a74792a21631f27eb5ad2156c33f025eb7f8a2eb
rajeev-k47 <rajeevkumawat2007@outlook.com>
2024-07-03 14:29:46 +0530

    <*/*>?

---
+++ app/src/main/java/com/example/ruine/Rvmodels/Rv_mail_model.kt
@@ -0,0 +1,7 @@
+package com.example.ruine.Rvmodels
+
+import com.example.ruine.R
+
+data class Rv_mail_model(val mail_profile:Int?= R.drawable.person, var messageId:String, var mail_title:String?="", var mail_date:String?="", var mail_snippet:String?="", var NewTag:Boolean?=false){
+
+}
---
+++ app/src/main/java/com/example/ruine/Rvmodels/Rvmodel.kt
@@ -0,0 +1,6 @@
+package com.example.ruine.Rvmodels
+
+import com.example.ruine.R
+
+data class  Rvmodel(var profile:Int?= R.drawable.group, var name: String?="", var mail_tag: String?="", val grp_id:String?="") {
+}
