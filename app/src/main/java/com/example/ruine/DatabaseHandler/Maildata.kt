7a5873fdbf79a49bbd620e07f107a176c4077cc0
rajeev-k47 <rajeevkumawat2007@outlook.com>
2024-07-13 14:07:14 +0530

    <Feat:Adds labelID to maildata>?

--- app/src/main/java/com/example/ruine/DatabaseHandler/Maildata.kt
+++ app/src/main/java/com/example/ruine/DatabaseHandler/Maildata.kt
@@ -2,7 +2,8 @@ package com.example.ruine.DatabaseHandler
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import org.json.JSONArray
 
 @Entity(tableName = "Mail_Data")
 
-data class Maildata(@PrimaryKey(autoGenerate=true)val Sr:Long, val Id:String?="",val messageId:String,val Title: String?="",val Date:String?="",val Subject:String?="")
+data class Maildata(@PrimaryKey(autoGenerate=true)val Sr:Long, val Id:String?="",val messageId:String,val Title: String?="",val Date:String?="",val Subject:String?="",val labelIds:JSONArray)
