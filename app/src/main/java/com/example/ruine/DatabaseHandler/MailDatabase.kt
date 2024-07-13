7746c55b2231e0296c6d03a7d1900a232fbc94a1
rajeev-k47 <rajeevkumawat2007@outlook.com>
2024-07-13 14:07:32 +0530

    <Feat:Adds typeConvertor>?

--- app/src/main/java/com/example/ruine/DatabaseHandler/MailDatabase.kt
+++ app/src/main/java/com/example/ruine/DatabaseHandler/MailDatabase.kt
@@ -2,8 +2,10 @@ package com.example.ruine.DatabaseHandler
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 
 @Database(entities = [Maildata::class], version = 1)
+@TypeConverters(Converters::class)
 abstract class MailDatabase:RoomDatabase (){
     abstract fun mailDao(): MailDao
 }
